0fe48cf5d21808b399d0994a5762e38e219d45ac
Julius Stopforth <jjstopforth@gmail.com>
2022-02-07 17:23:13 +0200

    update gosurf to include llands

--- bin/gosurf.sh
+++ bin/gosurf.sh
@@ -2,12 +2,13 @@
 
 milnerton="https://s46.ipcamlive.com/streams/2etknjm7zcxhpy2fs/stream.m3u8"
 muizenberg="https://cams.cdn-surfline.com/cdn-int/za-muizenberg/playlist.m3u8"
+llands="https://cams.cdn-surfline.com/cdn-int/za-llanduno/playlist.m3u8"
 
-if [[ $1 == "muizenberg" ]]; then
-    stream=$muizenberg
-else
-    stream=$milnerton
-fi
+case $1 in
+    "milnerton") stream=$milnerton;;
+    "muiz") stream=$muizneberg;;
+    "llands") stream=$llands;;
+esac
 
 mpv $stream
 
